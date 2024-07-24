a9f376b07a4818e2994ae078e9a85b99b3f8db47
solmysoul1 <solmysoul11@gmail.com>
2024-07-25 04:59:39 +0900

    hansol_20240724

---
+++ 00 Weekly Study/hansol_A나누기B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class A나누기B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        
+        System.out.print(a/b);
+    }
+}
