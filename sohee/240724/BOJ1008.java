9868f57a00e72cf6299e87fd8182493100163b91
242lee <iamurmaster_@naver.com>
2024-07-24 22:38:12 +0900

    :white_check_mark: A/B

--- sohee/240724/BOJ1008.java
+++ sohee/240724/BOJ1008.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class BOJ1008 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        double A = in.nextDouble();
-        double B = in.nextDouble();
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
         System.out.println(A / B);
+
+        scanner.close();
+        // close()를 안 쓰면 .. Resource leak : 'scanner' is never closed
     }
 }
